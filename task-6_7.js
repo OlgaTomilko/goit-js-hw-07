51cad6592fdbf025abb34db39d285950555489b6
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2020-11-15 01:22:14 +0300

    task-6

---
+++ task-6_7.js
@@ -0,0 +1,21 @@
+//Напиши скрипт, который бы при потере фокуса на инпуте, 
+//проверял его содержимое на правильное количество символов.
+
+//Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+//Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
+
+//Для добавления стилей, используй CSS-классы valid и invalid.
+
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', onInputBlur);
+
+
+function onInputBlur() {
+    input.classList.add('invalid');
+  if (input.value.length === Number(input.dataset.length)) {
+      input.classList.remove('invalid'); 
+      input.classList.add('valid');
+    };
+  };
+  
